19d967b5e5cb03fffe9a8d73a37d7009e291e29a
BG <amsinghnavdeep@gmail.com>
2026-05-05 16:59:59 +0000

    Add lazy-loaded routes for flights, hotels, cars, and holidays

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,25 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  {
+    path: 'flights',
+    loadComponent: () =>
+      import('./pages/flights/flights.component').then(m => m.FlightsComponent)
+  },
+  {
+    path: 'hotels',
+    loadComponent: () =>
+      import('./pages/hotels/hotels.component').then(m => m.HotelsComponent)
+  },
+  {
+    path: 'cars',
+    loadComponent: () =>
+      import('./pages/cars/cars.component').then(m => m.CarsComponent)
+  },
+  {
+    path: 'holidays',
+    loadComponent: () =>
+      import('./pages/holidays/holidays.component').then(m => m.HolidaysComponent)
+  },
   { path: '**', redirectTo: '' }
 ];
