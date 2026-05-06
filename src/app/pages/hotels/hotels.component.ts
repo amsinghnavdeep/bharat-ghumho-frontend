ef9ec9e425aeb5b5968ed97384c0fbdf81d6f77e
BG <amsinghnavdeep@gmail.com>
2026-05-06 01:15:08 +0000

    fix: Car[] and Hotel[] return types changed to any[] to resolve NG9 build errors

--- src/app/pages/hotels/hotels.component.ts
+++ src/app/pages/hotels/hotels.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+any[]import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
