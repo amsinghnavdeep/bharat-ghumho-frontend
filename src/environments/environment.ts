0dbadacf9e8f5ca58bd1371d4daccebcf5ce52bf
Nav Singh <amsinghnavdeep@gmail.com>
2026-05-07 15:57:29 -0400

    cleanup: remove duplicate readme

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,1 @@
-export const environment = { production: false, apiUrl: '/api' };
+export const environment = { production: false, apiUrl: 'https://bharat-ghumho-backend-production.up.railway.app' };
